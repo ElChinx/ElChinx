63e29514db3437c91a26c070dd40356b02fa46cf
= <=>
2022-04-11 00:24:40 -0500

    Menu Burger

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ var on = false;
 burger.addEventListener("click",burgerSH);
 
 function burgerSH(){
-    let i =  document.getElementById("nav-ul").querySelectorAll('li.i');
+    let i = document.getElementById("nav-ul").querySelectorAll('li.i');
     i.forEach(e => {
-        if(on){
-            e.className = 'i off';
-            e.removeEventListener("click",close);
-        }else{
-            e.className = 'i on';
+        if(!on){
+            e.style.display = 'flex';
             e.addEventListener("click",close)
+        }else{
+            e.style.display = 'none';
+            e.removeEventListener("click",close);
         }
     });
     if(on){
@@ -23,11 +23,27 @@ function burgerSH(){
 }
 
 function close(){
-    let i =  document.getElementById("nav-ul").querySelectorAll('li.on');
+    let i =  document.getElementById("nav-ul").querySelectorAll('li.i');
     i.forEach(e => {
             e.style.display = 'none';
             e.removeEventListener("click",close);
     });
     burger.innerHTML = "&#9776;";
+    on = false;
+  
+}
+
+function rsz(){
+    if (screen.width >= 1024) {
+        let i =  document.getElementById("nav-ul").querySelectorAll('li.i');
+        i.forEach(e => {
+                e.style.display = 'inline-block';
+                e.removeEventListener("click",close);
+        })
+        burger.innerHTML = "&#9776;";
         on = false;
+    }else {
+        on = true;
+        burgerSH()
+    }
 }
